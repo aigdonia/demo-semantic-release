0f87d1d1252e7eaf8848b7d9de27959325fa7eff
Ahmed Gaber <aigdonia@gmail.com>
2022-03-22 18:36:54 +0200

    feat:adding subtract method (#2)

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,6 +1,6 @@
 const test = require('ava')
 
-const {add, multiply} = require('../src/index')
+const {add, multiply, subtract} = require('../src/index')
 
 test('add should add two numbers', t => {
     const exptected = 3;
@@ -9,4 +9,8 @@ test('add should add two numbers', t => {
 
 test('multiply should cross two numbers', t => {
     t.is(multiply(2,3), 6)
+})
+
+test('subtract should subtract two numbers', t => {
+    t.is(subtract(2,3), -1)
 })
